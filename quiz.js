aa0b68b2942703fbb326181bd557e55b4289f51f
Willy <willy718066@gmail.com>
2023-02-19 19:56:13 +0800

    change to quiz

--- quiz.js
+++ quiz.js
@@ -1,14 +1,132 @@
-const senior = document.querySelector('.senior');
-// const wrap = document.querySelector('.wrap')
+const quizData = [
+  {
+    question: "   ",
+    a: "   ",
+    b: "   ",
+    c: "   ",
+    d: "   ",
+    correct: ""
+  },
+  {
+    question: "   ",
+    a: "   ",
+    b: "   ",
+    c: "   ",
+    d: "   ",
+    correct: ""
+  },
+  {
+    question: "   ",
+    a: "   ",
+    b: "   ",
+    c: "   ",
+    d: "   ",
+    correct: ""
+  },
+  {
+    question: "   ",
+    a: "   ",
+    b: "   ",
+    c: "   ",
+    d: "   ",
+    correct: ""
+  },
+  {
+    question: "   ",
+    a: "   ",
+    b: "   ",
+    c: "   ",
+    d: "   ",
+    correct: ""
+  },
+  {
+    question: "   ",
+    a: "   ",
+    b: "   ",
+    c: "   ",
+    d: "   ",
+    correct: ""
+  },
+  {
+    question: "   ",
+    a: "   ",
+    b: "   ",
+    c: "   ",
+    d: "   ",
+    correct: ""
+  },
+  {
+    question: "   ",
+    a: "   ",
+    b: "   ",
+    c: "   ",
+    d: "   ",
+    correct: ""
+  }
+];
 
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
 
-senior.addEventListener('click', function(e){
-    console.log(e.target.nodeName);
-    if((e.target.nodeName)=="SPAN"){
-  alert("要畢業囉~加油!");
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
 }
-  else{
-    alert('試試點其他地方');
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
 }
+
+function getSelected() {
+  let answer;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
 }
-)
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) { 
+          
+     score++;
+      
+    }if (answer !== quizData[currentQuiz].correct) { 
+            alert('請仔細訂正!');
+    
+      
+    }
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+            <h2>你一共答對了${score}/${quizData.length} 題</h2>
+
+            <button onclick="location.reload()">重來</button>`;
+    }
+  }
+});
